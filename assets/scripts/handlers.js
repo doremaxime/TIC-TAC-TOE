948093b4dac0d5623ed5b66396e57dffd46f6c11
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-01-27 13:07:41 -0500

    create handlers.js for jquery will hold all click functions

---
+++ assets/scripts/handlers.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+$(() => {
+  $('circle').on('click', function () {
+    let message = $('<span>x>');
+    $('.id4').append(message);
+    $(this).remove();
+  });
+  $('').on('click', function () {
+
+  });
+  $('').on('click', function () {
+
+  });
+});
+
+
+
+$(document).ready(function() {
+  $('square').on('click', function() {
+    let message = $('<span>x>');
+    $('.id4').append(message);
+    $(this).remove();
+  });
+});
